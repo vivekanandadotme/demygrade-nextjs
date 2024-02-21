387e4a2dfad54b9553839074daaefb6b1a6d386d
ADNAAN18 <mohammadadnaan2003@GMAIL.COM>
2024-02-21 23:07:06 +0530

    Header part commit

--- components/Header/Category/Category.js
+++ components/Header/Category/Category.js
@@ -5,17 +5,17 @@ import SingleCategory from "./CategoryProps/SingleCategory";
 const Category = () => {
   return (
     <div className="rbt-category-menu-wrapper rbt-category-update">
-      <div className="rbt-category-btn">
+      <div className="">
         <div className="rbt-offcanvas-trigger md-size icon">
           <span className="d-none d-xl-block">
-            <i className="feather-grid"></i>
+            {/* <i className="feather-grid"></i> */}
           </span>
-          <i title="Category" className="feather-grid d-block d-xl-none"></i>
+          {/* <i title="Category" className="feather-grid d-block d-xl-none"></i> */}
         </div>
-        <span className="category-text d-none d-xl-block">Category</span>
+        {/* <span className="category-text d-none d-xl-block">Category</span> */}
       </div>
 
-      <div className="update-category-dropdown">
+      {/* <div className="update-category-dropdown">
         <div className="inner">
           <ul className="dropdown-parent-wrapper">
             <SingleCategory
@@ -41,7 +41,7 @@ const Category = () => {
             </li>
           </ul>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
--- components/Header/Header-Right/HeaderRight-Two.js
+++ components/Header/Header-Right/HeaderRight-Two.js
@@ -1,7 +1,8 @@
 import Link from "next/link";
 
 import { useSelector } from "react-redux";
-
+import ColorButton from "../../Button/ButtonProps/ColorButton";
+import SectionHead from "../../Button/ButtonProps/SectionHead";
 import User from "../Offcanvas/User";
 import { useAppContext } from "@/context/Context";
 
@@ -14,7 +15,7 @@ const HeaderRightTwo = ({ btnClass, btnText, userType }) => {
   return (
     <div className="header-right">
       <ul className="quick-access">
-        <li className="access-icon">
+        {/* <li className="access-icon">
           <Link
             className={`search-trigger-active rbt-round-btn ${
               search ? "" : "open"
@@ -24,9 +25,9 @@ const HeaderRightTwo = ({ btnClass, btnText, userType }) => {
           >
             <i className="feather-search"></i>
           </Link>
-        </li>
+        </li> */}
 
-        <li className="access-icon rbt-mini-cart">
+        {/* <li className="access-icon rbt-mini-cart">
           <Link
             className="rbt-cart-sidenav-activation rbt-round-btn"
             href="#"
@@ -35,29 +36,48 @@ const HeaderRightTwo = ({ btnClass, btnText, userType }) => {
             <i className="feather-shopping-cart"></i>
             <span className="rbt-cart-count">{total_items}</span>
           </Link>
-        </li>
+        </li> */}
 
-        <li className="account-access rbt-user-wrapper d-none d-xl-block">
+        {/* <li className="account-access rbt-user-wrapper d-none d-xl-block">
           <Link href="#">
             <i className="feather-user"></i>
             {userType}
           </Link>
           <User />
-        </li>
+        </li> */}
 
-        <li className="access-icon rbt-user-wrapper d-block d-xl-none">
+        {/* <li className="access-icon rbt-user-wrapper d-block d-xl-none">
           <Link className="rbt-round-btn" href="#">
             <i className="feather-user"></i>
           </Link>
           <User />
-        </li>
+        </li> */}
       </ul>
 
-      <div className="rbt-btn-wrapper d-none d-xl-block">
-        <Link className={`rbt-btn ${btnClass}`} href="#">
+      {/* <div className="rbt-btn-wrapper d-none d-xl-block">
+        <Link className={`rbt-btn`} href="#">
           <span data-text={`${btnText}`}>{btnText}</span>
         </Link>
-      </div>
+      </div> */}
+
+<ColorButton
+        head={
+          <SectionHead
+            textBg="bg-primary-opacity"
+            title="Border Variation."
+            text="Button Border"
+          />
+        }
+        btnText="Histudy Button"
+        // classOne="rbt-button-area rbt-section-gapBottom bg-color-white"
+        // btnClassOne="btn-border square"
+        // btnClassTwo="btn-border"
+        // btnClassThree="btn-border radius-round-6"
+        // btnClassFour="btn-border radius-round-10"
+        btnClassFive="btn-border radius-round"
+        lastbtn={false}
+      />
+      
 
       <div className="mobile-menu-bar d-block d-xl-none">
         <div className="hamberger">
