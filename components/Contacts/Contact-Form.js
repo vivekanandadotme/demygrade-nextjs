3a06ca53896491811e81e2b05644b4e436f90201
Vivekananda Nareddula <vivek.nrt@gmail.com>
2024-03-13 14:43:21 +0530

    Did changes to make contact us Work

--- components/Contacts/Contact-Form.js
+++ components/Contacts/Contact-Form.js
@@ -23,7 +23,7 @@ const ContactForm = ({ gap }) => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('/api/submitForm', {
+      const response = await fetch('http://localhost:3001/api/submitForm', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
